b21605157f3d1f99107855820190cfffe94760e2
Antonio Fry <antoniofry2000@gmail.com>
2019-08-19 15:06:01 -0600

    run migration and initialize seed file

--- db/migrations/20190819142744_projects.js
+++ db/migrations/20190819142744_projects.js
@@ -16,7 +16,8 @@ exports.up = function(knex) {
       table.string('color_4');
       table.string('color_5');
       table.integer('project_id').unsigned();
-      table.foreign('project_id').refrences('projects.id');
+      table.foreign('project_id')
+        .references('projects.id');
 
       table.timestamps(true, true);
     })
