89cc027e5c80ae750bcba62c1ad810425ee2a6fd
Katherine Lewis <kale8432@colorado.edu>
2019-08-20 14:08:03 -0600

    added production env

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,8 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
-  },
+  }
+
 
   test: {
      client: 'pg',
@@ -24,22 +25,18 @@ module.exports = {
        directory: './db/seeds/test'
      },
      useNullAsDefault: true,
-   },
+   }
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+   production: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL + '?ssl=true',
+   migrations: {
+     directory: './db/migrations'
+   },
+   seeds: {
+     directory: './db/seeds/dev'
+   },
+   useNullAsDefault: true,
+ }
 
 };
