53cfdf5dd53fda8e4e6a69023b1cc1709d2a8715
Antonio Fry <antoniofry2000@gmail.com>
2019-08-22 15:21:19 -0600

    finish majority of tests

--- app.test.js
+++ app.test.js
@@ -129,7 +129,6 @@ describe('App', () => {
         }
 
         const response = await request(app).post('/api/v1/palettes').send(newPalette);
-        console.log(response.body)
         const palettes = await database('palettes').where('id', response.body[0]).select();
         const palette = palettes[0];
 
@@ -164,54 +163,142 @@ describe('App', () => {
       });
     });
   
-    describe('DELETE /project', () => {
-      it('should delete a given project', () => {
-  
-      });
-      it('Happy Path: return a status of 202', () => {
-  
-      });
-      it('Sad Path: return a status of 404', () => {
-  
-      });
-    });
-  
-    describe('DELETE /palette', () => {
-      it('should delete a given palette', () => {
-  
-      });
-      it('Happy Path: return a status of 200', () => {
-  
+    describe('DELETE /api/v1/projects', () => {
+      it('Happy Path: return a status of 202', async () => {
+        const projectId = await database('projects').first('id');
+        const response = await request(app).delete(`/api/v1/projects/${projectId.id}`)
+
+        expect(response.status).toBe(204)
       });
       it('Sad Path: return a status of 404', () => {
   
       });
     });
   
-    describe('Patch /project', () => {
-      it('should update a project', () => {
+    describe('DELETE /api/v1/palettes', () => {
+      it('Happy Path: return a status of 200', async () => {
+        const paletteId = await database('palettes').first('id');
+        const response = await request(app).delete(`/api/v1/palettes/${paletteId.id}`)
   
-      });
-      it('Happy Path: return a status of 200', () => {
-  
+        expect(response.status).toBe(204)
       });
       it('Sad Path: return a status of 404', () => {
   
       });
     });
   
-    describe('Patch /palette', () => {
-      it('should update a palette', () => {
-  
+    describe('PATCH /api/v1/project/:id', () => {
+      it('should update a project', async () => {
+        const updatedProjects = {
+          name: "Dog Party",
+        }
+
+        const response = await request(app)
+          .patch("/api/v1/projects/1")
+          .send(updatedProjects);
+        const projects = await database('projects').where('id', 1).select();
+        const project = projects[0];
+
+
+        expect(project.name).toEqual(updatedProjects.name);
       });
-      it('Happy Path: return a status of 200', () => {
-  
+      it('Happy Path: return a status of 200', async () => {
+        const updatedProjects = {
+          name: "Dog Party",
+        }
+
+        const response = await request(app)
+          .patch("/api/v1/projects/1")
+          .send(updatedProjects);
+
+        expect(response.status).toBe(200);
+        expect(response.text).toBe("Project successfully updated");
       });
-      it('Sad Path: return a status of 404', () => {
-  
+      it('should return a status of 422 if missing parameter', async () => {
+        const updatedProjects = {
+          name: null
+        }
+        const response = await request(app)
+          .patch("/api/v1/projects/6")
+          .send(updatedProjects);
+
+        expect(response.status).toBe(422);
       });
+      it('SAD PATH: should return status of 404', async () => {
+        const updatedProjects = {
+          name: "Dog Party",
+        }
+
+        const response = await request(app)
+          .patch("/api/v1/projects/6")
+          .send(updatedProjects);
+
+        expect(response.status).toBe(404)
+      })
     });
 
-  }); 
+    describe('PATCH /api/v1/palette/:id', () => {
+      it('should update a palette', async () => {
+        const updatedPalette = {
+          palette_name: "Boring Colors",
+          color_1: "#800000",
+          color_2: "#FC1501",
+          color_3: "#F87531",
+          color_4: "#FF8000",
+          color_5: "#FFCC11"
+        }
+        const response = await request(app)
+          .patch("/api/v1/palettes/1")
+          .send(updatedPalette);
+        const palettes = database('palettes').where("project_id", 1).select();
 
+        const palette = palettes[0];
+        
+        expect(palette.palette_name).toEqual(updatedPalette.palette_name);
+      });
+      it('Happy Path: return a status of 200', async () => {
+        const updatedPalette = {
+          palette_name: "Boring Colors",
+          color_1: "#809000",
+          color_2: "#FC1501",
+          color_3: "#F87531",
+          color_4: "#FF8000",
+          color_5: "#FFCC11"
+        }
+  
+        const response = await request(app).patch('/api/v1/palettes/1').send(updatedPalette);
+
+        expect(response.status).toBe(200);
+        expect(response.text).toBe("Palette successfully updated");
+      });
+      it('Sad Path: return a status of 404', async () => {
+        const updatedPalette = {
+          palette_name: "Boring Colors",
+          color_1: "#809000",
+          color_2: "#FC1501",
+          color_3: "#F87531",
+          color_4: "#FF8000",
+          color_5: "#FFCC11"
+        }
+
+        const response = await request(app).patch('/api/v1/palettes/6').send(updatedPalette);
+
+        expect(response.status).toBe(404)
+      });
+
+      it('should return a status of 422 if missing parameter', async () => {
+        const updatedPalette = {
+          color_1: "#800000",
+          color_3: "#F87531",
+          color_4: "#FF8000",
+          color_5: "#FFCC11"
+        }
+        const response = await request(app)
+          .patch("/api/v1/palettes/1")
+          .send(updatedPalette);
+        expect(response.status).toBe(422);
+      })
+    });
+  });
 });
+       //change in app.js
