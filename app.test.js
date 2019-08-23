1507c1a1e4be70e56588ec57e6f643c142b9a943
Antonio Fry <antoniofry2000@gmail.com>
2019-08-22 21:57:45 -0600

    passing all tests

--- app.test.js
+++ app.test.js
@@ -243,22 +243,22 @@ describe('App', () => {
 
     describe('PATCH /api/v1/palette/:id', () => {
       it('should update a palette', async () => {
-        // const updatedPalette = {
-        //   palette_name: "Boring Colors",
-        //   color_1: "#800000",
-        //   color_2: "#FC1501",
-        //   color_3: "#F87531",
-        //   color_4: "#FF8000",
-        //   color_5: "#FFCC11"
-        // }
-        // const response = await request(app)
-        //   .patch("/api/v1/palettes/1")
-        //   .send(updatedPalette);
-        // const palettes = database('palettes').where("project_id", 1).select();
-
-        // const palette = palettes[0];
+        const updatedPalette = {
+          palette_name: "Boring Colors",
+          color_1: "#800000",
+          color_2: "#FC1501",
+          color_3: "#F87531",
+          color_4: "#FF8000",
+          color_5: "#FFCC11"
+        }
+        const response = await request(app)
+          .patch("/api/v1/palettes/1")
+          .send(updatedPalette);
+        const palettes = await database('palettes').where("id", 1).select();
+        console.log(palettes)
+        const palette = palettes[0];
         
-        // expect(palette.palette_name).toEqual(updatedPalette.palette_name);
+        expect(palette.palette_name).toEqual(updatedPalette.palette_name);
       });
       it('Happy Path: return a status of 200', async () => {
         
